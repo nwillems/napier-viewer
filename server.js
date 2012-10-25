a7cbd7aa3010ec32cf1d5fe1e312aa4d943c708c
Nicolai Willems <niwil90@gmail.com>
2012-10-26 00:02:58 +0200

    [Static]Change from node-static to ecstatic. DirListing

--- server.js
+++ server.js
@@ -1,37 +1,33 @@
-var stat = require('node-static');
 var fs = require('fs');
 var path = require('path');
-var file = new(stat.Server)('./log');
-var file2 = new(stat.Server)(path.dirname(__filename));
+var ecstatic = require('ecstatic');
+var file = ecstatic('./log', {
+    autoIndex : true,
+    cache : 30
+});
+var file2 = ecstatic(path.dirname(__filename), { autoIndex: false });
 
 require('http').createServer(function(request, response){
     request.addListener('end', function(){
         console.log("Received request for: ", request.url);
-        console.log("DEBUG", request.url.substring(0,5));
+        if(request.url === '/view'){
+            response.writeHead(301, { 'location' : '/view/'});
+            return response.end();
+        }
+
         if(request.url.substring(0, 5) === '/view'){
+            console.log("Incomming request for: ", request.url);
             var url = request.url;
             request.url = url.substring(5, url.length);
             if(request.url === '') request.url = 'index.html';
-            console.log('URL is now: ', request.url);
             
-            file2.serve(request, response); 
-        }else if(request.url === '/'){
-            //Serve up a file list
-            fs.readdir('./log', function(err, files){
-                if(err){
-                    response.writeHead(500);
-                    return response.write(err.toString());
-                }
-                response.write('<html><body><ul>');
-                for(var i = 0; i < files.length; i++){
-                    response.write('<li>');
-                    response.write('<a href="'+files[i]+'">'+files[i]+'</a>');
-                    response.write('</li>');
-                }
-                response.end('</ul></body></html>');
+            file2(request, response, function(){
+                console.log("Request for view", arguments);
+            }); 
+        }else{ 
+            file(request, response, function(){
+                console.log("Request for logs", arguments);
             });
-        }else{
-            file.serve(request, response);
         }
     });
 }).listen(7890);
